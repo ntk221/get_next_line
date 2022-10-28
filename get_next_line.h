5c215d996f5d91e7dae2698759589eaad49728fd
Kazuki Nitta <knitta@c1r13s2.42tokyo.jp>
2022-10-28 20:43:06 +0900

    norm checked on guacamole

--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,8 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
@@ -13,8 +13,7 @@ char	*ft_strdup(const char *src);
 char	*ft_strchr(char *str, int c);
 char	*get_next_line(int fd);
 char	*erase_newline(char *note);
-char    *get_line(char *str);
+char	*get_line(char *str);
 char	*read_until_newline(int fd, char *note);
-
 
 #endif
